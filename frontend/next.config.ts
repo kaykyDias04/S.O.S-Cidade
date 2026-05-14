277bbdb12d3b8828ed222ea319868dcbfc217122
Kayky Dias <kdo@cesar.school>
2026-05-14 12:19:49 -0300

    fix: ajustes no turbopack p deploy

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   experimental: {
-    turbopack: {},
+    turbopackFileSystemCacheForDev: true,
   },
   webpack: (config, context) => {
     config.watchOptions = {
